44b89bb993d4c5c6db735b21f2fdffb3aa33b48f
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-17 14:40:09 -0400

    asd

--- src/components/CharacterModal.jsx
+++ src/components/CharacterModal.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+// src/components/CharacterModal.js
+import React from 'react';
+import './styles/CharacterModal.css';
 
-const CharacterModal = () => {
+const CharacterModal = ({ character, onClose }) => {
+  const closeModalOnOverlayClick = (e) => {
+    if (e.target.className === 'modal-overlay') {
+      onClose();
+    }
+  };
+
   return (
-    <div>CharacterModal</div>
-  )
-}
+    <div className="modal-overlay" onClick={closeModalOnOverlayClick}>
+      <div className="modal-content">
+        <button className="close-modal" onClick={onClose}>
+          &times;
+        </button>
+        <h2>{character.name}</h2>
+        <img src={character.image} alt={character.name} />
+        <p>Status: {character.status}</p>
+        <p>Species: {character.species}</p>
+        <p>Gender: {character.gender}</p>
+        <p>Origin: {character.origin.name}</p>
+        <p>Last known location: {character.location.name}</p>
+      </div>
+    </div>
+  );
+};
 
-export default CharacterModal
+export default CharacterModal;
