7f094780ed4690cd1eddf01df063ff6bd981b43e
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-17 18:57:37 -0400

    asd

--- src/components/RandomLocation.jsx
+++ src/components/RandomLocation.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './styles/RandomLocation.css';
 
 const RandomLocation = ({ locationId, onLocationChange }) => {
   const [location, setLocation] = useState(null);
@@ -33,21 +34,33 @@ const RandomLocation = ({ locationId, onLocationChange }) => {
   
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div className="random-location"><div className="loading">Error: {error.message}</div></div>;
   }
 
   return (
-    <div>
+    <div className="random-location">
       {location ? (
         <div>
           <h2>Ubicación</h2>
-          <p>Nombre: {location.name}</p>
-          <p>Tipo: {location.type}</p>
-          <p>Dimensión: {location.dimension}</p>
-          <p>Población: {location.residents.length}</p>
+          <div className="info">
+            <span className="label">Nombre:</span>
+            <span className="value">{location.name}</span>
+          </div>
+          <div className="info">
+            <span className="label">Tipo:</span>
+            <span className="value">{location.type}</span>
+          </div>
+          <div className="info">
+            <span className="label">Dimensión:</span>
+            <span className="value">{location.dimension}</span>
+          </div>
+          <div className="info">
+            <span className="label">Población:</span>
+            <span className="value">{location.residents.length}</span>
+          </div>
         </div>
       ) : (
-        <p>Cargando...</p>
+        <div className="loading">Cargando...</div>
       )}
     </div>
   );
